35ca23d375a9a593976883d244c3d2164d3b9e41
Igor <igor.ushkarev1@justice.gov.uk>
2026-06-01 17:16:35 +0100

    Keep older swagger ui dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
   // OpenAPI
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
   constraints {
-    implementation("org.webjars:swagger-ui:5.32.6")
+    implementation("org.webjars:swagger-ui:5.32.2")
   }
 
   // Persistence
