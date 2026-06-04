85abec7d955e3ab94e4232f7619875387fc0245d
ushkarev <ushkarev@users.noreply.github.com>
2026-06-04 14:01:39 +0100

    Update infrastructure setup (#9)
    * Replace snyk api token with oauth (hmpps-template-kotlin#359)
    * Update infrastructure dependencies in one batch (now including docker images and helm charts) and add 7 day cooldown to gradle updates
    * Add error reporting to Sentry.io
    * Set timezone for all scheduled workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
 
+  // Monitoring
+  implementation("io.sentry:sentry-spring-boot-4:8.43.0")
+
   // OpenAPI
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
   constraints {
