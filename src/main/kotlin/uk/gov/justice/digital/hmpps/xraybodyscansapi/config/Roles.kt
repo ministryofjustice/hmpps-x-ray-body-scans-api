279f57320b2a44ed40b938c48d1cf547a3c1c38f
Igor <igor.ushkarev1@justice.gov.uk>
2026-06-24 23:05:07 +0100

    Factor out role pre-authorisation checks

--- src/main/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/config/Roles.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/config/Roles.kt
@@ -1,7 +1,15 @@
 package uk.gov.justice.digital.hmpps.xraybodyscansapi.config
 
+import org.springframework.security.access.prepost.PreAuthorize
+
 /** Read-only access to x-ray body scans */
 const val ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RO = "ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RO"
 
 /** Read-write access to x-ray body scans */
 const val ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RW = "ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RW"
+
+@PreAuthorize("hasAnyRole('$ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RO', '$ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RW')")
+annotation class RequireReadRole
+
+@PreAuthorize("hasRole('$ROLE_X_RAY_BODY_SCANS_API__SCAN_DATA__RW')")
+annotation class RequireWriteRole
