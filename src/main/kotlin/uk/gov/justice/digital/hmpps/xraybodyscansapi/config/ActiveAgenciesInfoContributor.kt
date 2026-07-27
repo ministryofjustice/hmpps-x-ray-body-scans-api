0ed1b6218ac176ee11f78194e358e5e99e41201e
Igor <igor.ushkarev1@justice.gov.uk>
2026-07-27 12:05:18 +0100

    List active agencies in /info endpoint – currently configured in application yaml but will come from database in future

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/config/ActiveAgenciesInfoContributor.kt
@@ -0,0 +1,17 @@
+package uk.gov.justice.digital.hmpps.xraybodyscansapi.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.actuate.info.Info
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.stereotype.Component
+
+@Component
+class ActiveAgenciesInfoContributor(
+  // TODO: currently saved in yaml configuration but will move to database to store dates when each prison goes live
+  @param:Value($$"${service.active-agencies}")
+  val activeAgencies: List<String>,
+) : InfoContributor {
+  override fun contribute(builder: Info.Builder) {
+    builder.withDetail("activeAgencies", activeAgencies)
+  }
+}
--- src/test/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/integration/health/InfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/integration/health/InfoTest.kt
@@ -28,4 +28,12 @@ class InfoTest : IntegrationTestBase() {
         assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
       }
   }
+
+  @Test
+  fun `Info page lists active agencies`() {
+    webTestClient.get().uri("/info")
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("activeAgencies").isEqualTo(listOf("***"))
+  }
 }
