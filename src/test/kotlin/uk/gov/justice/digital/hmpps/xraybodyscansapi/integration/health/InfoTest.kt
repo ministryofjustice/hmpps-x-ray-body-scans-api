5c065ad798dbba54454e8f4c4167a78b86574a86
ushkarev <ushkarev@users.noreply.github.com>
2026-07-27 15:06:50 +0100

    List active agencies in /info endpoint (#48)
    We’ll need to indicate where this service has rolled out so this is a stub to say “everywhere” except in production where it’s “nowhere”.
    
    Currently configured in application yaml but will come from database in future. The reason being that we shall have to know the live dates in order to calculate scan counts.
    
    based on [case notes](https://github.com/ministryofjustice/offender-case-notes/blob/0b80c68864364631a36355580e49742d48947436/src/main/java/uk/gov/justice/hmpps/casenotes/config/ServiceConfigInfo.kt) and could later use ideas from [prisoner-property](https://github.com/ministryofjustice/hmpps-prisoner-property-api/pull/40) for the database version

--- src/test/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/integration/health/InfoTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/integration/health/InfoTest.kt
@@ -2,10 +2,12 @@ package uk.gov.justice.digital.hmpps.xraybodyscansapi.integration.health
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.test.context.TestPropertySource
 import uk.gov.justice.digital.hmpps.xraybodyscansapi.integration.IntegrationTestBase
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
+@TestPropertySource(properties = ["service.active-agencies=LEI,MDI"])
 class InfoTest : IntegrationTestBase() {
 
   @Test
@@ -27,5 +29,13 @@ class InfoTest : IntegrationTestBase() {
       .expectBody().jsonPath("build.version").value<String> {
         assertThat(it).startsWith(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE))
       }
+  }
+
+  @Test
+  fun `Info page lists active agencies`() {
+    webTestClient.get().uri("/info")
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("activeAgencies").isEqualTo(listOf("LEI", "MDI"))
   }
 }
