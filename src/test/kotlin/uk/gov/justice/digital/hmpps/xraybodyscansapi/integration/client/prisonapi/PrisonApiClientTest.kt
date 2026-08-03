1696c531f19a09988beb795101c2d54549bb4e5a
Igor <igor.ushkarev1@justice.gov.uk>
2026-08-03 15:55:13 +0100

    Use prison-api mock server extension like the one for hmpps-auth

--- src/test/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/integration/client/prisonapi/PrisonApiClientTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/xraybodyscansapi/integration/client/prisonapi/PrisonApiClientTest.kt
@@ -2,18 +2,19 @@ package uk.gov.justice.digital.hmpps.xraybodyscansapi.client.prisonapi
 
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import uk.gov.justice.digital.hmpps.xraybodyscansapi.client.prisonapi.response.PersonalCareNeed
 import uk.gov.justice.digital.hmpps.xraybodyscansapi.client.prisonapi.response.PersonalCareNeedsResponse
 import uk.gov.justice.digital.hmpps.xraybodyscansapi.config.DownstreamServiceException
-import uk.gov.justice.digital.hmpps.xraybodyscansapi.integration.wiremock.PrisonApiMockServer
+import uk.gov.justice.digital.hmpps.xraybodyscansapi.integration.wiremock.PrisonApiExtension
+import uk.gov.justice.digital.hmpps.xraybodyscansapi.integration.wiremock.PrisonApiExtension.Companion.prisonApi
 import java.time.LocalDate
 
+@ExtendWith(PrisonApiExtension::class)
 class PrisonApiClientTest {
   private lateinit var client: PrisonApiClient
 
@@ -21,14 +22,15 @@ class PrisonApiClientTest {
 
   @BeforeEach
   fun resetMocks() {
-    server.resetRequests()
-    val webClient = WebClient.create("http://localhost:${server.port()}")
+    prisonApi.resetRequests()
+    val webClient = WebClient.create("http://localhost:${prisonApi.port()}")
     client = PrisonApiClient(webClient)
   }
 
   @Test
   fun `getScanCareNeeds returns personal care needs for each offender`() {
-    server.stubGetScanCareNeeds(
+    prisonApi.stubGetScanCareNeeds(
+      // language=json
       """
       [
         {
@@ -69,7 +71,7 @@ class PrisonApiClientTest {
 
   @Test
   fun `getScanCareNeeds throws on 404`() {
-    server.stubGetScanCareNeedsError(404)
+    prisonApi.stubGetScanCareNeedsError(404)
 
     assertThatThrownBy { client.getScanCareNeeds(offenderNos) }
       .isInstanceOf(DownstreamServiceException::class.java)
@@ -81,7 +83,7 @@ class PrisonApiClientTest {
 
   @Test
   fun `getScanCareNeeds throws on 500`() {
-    server.stubGetScanCareNeedsError(500)
+    prisonApi.stubGetScanCareNeedsError(500)
 
     assertThatThrownBy { client.getScanCareNeeds(offenderNos) }
       .isInstanceOf(DownstreamServiceException::class.java)
@@ -89,22 +91,5 @@ class PrisonApiClientTest {
       .cause()
       .extracting { (it as WebClientResponseException).statusCode.value() }
       .isEqualTo(500)
-  }
-
-  companion object {
-    @JvmField
-    internal val server = PrisonApiMockServer()
-
-    @BeforeAll
-    @JvmStatic
-    fun startMocks() {
-      server.start()
-    }
-
-    @AfterAll
-    @JvmStatic
-    fun stopMocks() {
-      server.stop()
-    }
   }
 }
